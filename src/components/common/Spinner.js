f83d937f094f8d478453ffc273a0cb259301d5f8
Veselin <vesselin60@gmail.com>
2020-12-11 17:16:44 +0200

    333. Creating an Activity Spinner

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
 
-const Spinner = () => {
+const Spinner = ({ size }) => {
   return (
-
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={size || 'large'} />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export { Spinner };
