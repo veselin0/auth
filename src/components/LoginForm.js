5f7e85ed4697a7608cbc0a715e2677796655c3bb
Veselin <vesselin60@gmail.com>
2020-12-11 14:57:58 +0200

    329. Password Inputs

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,21 +2,29 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 export default class LoginForm extends Component {
-  state = { email: '' };
+  state = { email: '', password: '' };
 
   render() {
     return (
       <Card>
         <CardSection>
           <Input
-            placeholder="user@mail.com"
+            placeholder="user@email.com"
             label="Email"
             onChangeText={(email) => this.setState({ email })}
             value={this.state.email}
           />
         </CardSection>
 
-        <CardSection />
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={(password) => this.setState({ password })}
+          />
+        </CardSection>
 
         <CardSection>
           <Button>Log in</Button>
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { TextInput, View, Text, StyleSheet } from 'react-native';
 
-const Input = ({ label, value, onChangeText, placeholder }) => {
+const Input = ({
+  label,
+  value,
+  onChangeText,
+  placeholder,
+  secureTextEntry,
+}) => {
   const { inputStyle, labelStyle, containerStyle } = styles;
 
   return (
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
       <TextInput
+        secureTextEntry={secureTextEntry}
         placeholder={placeholder}
         autoCorrect={false}
         style={inputStyle}
